53e4a357bcb06c394a804458525d6c563cf48d9e
Angus Townsley <angustownsley@gmail.com>
2024-06-28 22:59:17 +0100

    Complete ERD and user entity

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,28 +1,32 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 async function seed() {
     const createdUsers = await prisma.user.createMany({
         data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
-        ]
-    });
+            {
+                username: 'alicemart',
+                email: 'alicemartin@email.com',
+                password: 'insecure',
+            },
+            {
+                username: 'allymartin',
+                email: 'allymartin@email.com',
+                password: 'insecure',
+            },
+        ],
+    })
 
-    console.log(`${createdUsers.count} users created`, createdUsers);
+    console.log(`${createdUsers.count} users created`, createdUsers)
 
     // Add your code here
 
-    
-
-
     // Don't edit any of the code below this line
-    process.exit(0);
+    process.exit(0)
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async (error) => {
+    console.error(error)
+    await prisma.$disconnect()
+    process.exit(1)
+})
